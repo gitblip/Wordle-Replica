0af09a1227eeace16cbf5c705e5fd4d2f0dbd1cd
gitblip <228890154+gitblip@users.noreply.github.com>
2026-09-04 17:41:48 +0000

    Changed db for threemin

--- database.js
+++ database.js
@@ -27,7 +27,7 @@ async function getScores() {
 
 async function submitScore(player, score) {
     const { data, error } = await supabaseClient
-        .from("scores")
+        .from("scores_threemin")
         .insert([
             {
                 player: player,
